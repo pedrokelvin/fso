ba7e825d1d717b0871838a6bede6b1204a28b86d
Matheus Roberto <matheus456456@gmail.com>
2018-06-04 00:46:59 -0300

    Repairing everything
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>

--- problema2/estudantes.c
+++ problema2/estudantes.c
@@ -8,12 +8,13 @@
 
 #define RAND 1
 
-int totalThreads = 10;
-int countThreads = 10;
+int totalThreads = 4;
+int countThreads = 4;
 sem_t sem_aluno;
 sem_t sem_monitor;
 sem_t sem_aviso;
 sem_t sem_filaEspera;
+sem_t sem_sendoAtendido;
 pthread_mutex_t mutex;
 pthread_mutex_t mutex_filaEspera;
 
@@ -26,21 +27,20 @@ typedef struct thread{
 void* monitor(void *pt) {
   while(countThreads > 1)
   {
-    printf("COUNT = %d\n", countThreads);
     sem_wait(&sem_aluno);
-    sleep(1);
+    // sleep(7);
     sem_post(&sem_monitor);
+
     // --countThreads;
     sem_wait(&sem_aviso);
   }
 }
 
 void* emAtendimento(void *pt){
+  sem_wait(&sem_sendoAtendido);
   Thread *pt_aux = (Thread *) pt;
-
   sem_wait(&sem_monitor);
-  sleep(2);
-  printf("O ALUNO %lu FOI ATENDIDO\n", pt_aux->id);
+  sleep(7);
   sem_post(&sem_aluno);
   if(pt_aux->filaEspera == 1)
   {
@@ -53,45 +53,54 @@ void* emAtendimento(void *pt){
     --countThreads;
 
   sem_post(&sem_aviso);
+  sem_post(&sem_sendoAtendido);
 }
 
 void* comportamentoAluno(void* pt){
   Thread *pt_aux = (Thread *) pt;
 
   while(pt_aux->count < 3){
-    int r = rand() % 2;
+    int r = rand()%2;
     int total;
+    int sendoAtendido;
     // printf("ESCOLHA: %d\n", r);
 
     switch(r){
       case 0:
-        printf("O ALUNO %lu FOI DORMIR\n", pt_aux->id);
-        sleep(1);
+        printf("O aluno %lu foi dormir\n", pt_aux->id);
+        sleep(7);
         // ficar em casa
       break;
 
       case 1:
-
-        sem_getvalue(&sem_filaEspera, &total);
-        printf("TOTAL = %d\n", total);
+        printf("ENTROU %lu\n", pt_aux->id);
         pthread_mutex_lock(&mutex_filaEspera);
-        if(total > 0 && total < (int)ceil(totalThreads/2.0))
+        sem_getvalue(&sem_filaEspera, &total);
+        sem_getvalue(&sem_sendoAtendido, &sendoAtendido);
+        // printf("Número de vagas na fila de espera = %d\n", total);
+
+
+        if(sendoAtendido == 0 && total > 0)
         {
+          printf("O aluno %lu entrou na fila de espera, na posição %d\n", pt_aux->id, ((int)ceil((totalThreads-1)/2.0) - total)+1);
           sem_wait(&sem_filaEspera);
-          printf("O ALUNO %lu ENTROU NA FILA DE ESPERA\n", pt_aux->id);
           pt_aux->filaEspera = 1;
         }
+
         else if(total <= 0)
         {
+          pthread_mutex_unlock(&mutex_filaEspera);
           break;
         }
         pthread_mutex_unlock(&mutex_filaEspera);
 
+
         pthread_mutex_lock(&mutex);
+        // printf("VALOR DO SENDOATENDIDO = %d\n", sendoAtendido);
+        printf("O aluno %lu está sendo atendido\n", pt_aux->id);
         emAtendimento(pt);
         pthread_mutex_unlock(&mutex);
-        // unlock mutex
-      break;
+        break;
     }
   }
 }
@@ -100,7 +109,8 @@ int main(){
   sem_init(&sem_aluno, 0, 0);
   sem_init(&sem_monitor, 0, 1);
   sem_init(&sem_aviso, 0, 0);
-  sem_init(&sem_filaEspera, 0, (int)ceil(totalThreads/2.0));
+  sem_init(&sem_filaEspera, 0, (int)ceil((totalThreads-1)/2.0));
+  sem_init(&sem_sendoAtendido, 0, 1);
   pthread_mutex_init(&mutex,NULL);
   pthread_mutex_init(&mutex_filaEspera,NULL);
 
@@ -122,7 +132,6 @@ int main(){
 
   for (int i = totalThreads-1; i >= 0; i--) {
     pthread_join(tids[i], NULL);
-    printf("ACABOU %i\n", i);
   }
 
 
