e40d9a8d5e485416872dc87f9af708a80e29ddcc
Matheus Roberto <matheus456456@gmail.com>
2018-06-14 17:22:51 -0300

    Implementing first functions

--- projeto2/questao.c
+++ projeto2/questao.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+int indexx=0;
+char codigo[33];
+
+void printInBinary(long num, int bit)
+{
+   if ( bit >= 4*sizeof(num) )
+   {
+      return;
+   }
+
+   printInBinary(num/2, bit+1);
+   codigo[indexx++] = num%2 + '0';
+}
 
 int main()
 {
-    long number;
-    if ( 1 != scanf("%ld", &number) )
-        return 1;
+   int y = 31;
+   int conteudo;
+   int pagina;
+   int deslocamento;
+   char aux[17];
 
-    // sign bit  (cannot use 1L left-shift as that causes UB)
-    putchar( '0' + (number < 0) );
+   uint32_t x1 = 1;
+   scanf(" %u", &x1);
+   printInBinary(x1, 0);
+   codigo[32] = '\0';
+   printf("%s\n", codigo);
 
-    // value bits
-    for (long bit = 1L << (CHAR_BIT * sizeof number - 2); bit; bit >>= 1)
-        putchar( '0' + !!(number & bit) );
-
-    putchar('\n');
+   strncpy(aux, codigo+16, 8);
+   aux[8] = '\0';
+   pagina = atoi(aux);
+   strncpy(aux, codigo+24, 8);
+   aux[8] = '\0';
+   deslocamento = atoi(aux);
+   printf("Pagina: %d\nDeslocamento: %d\n",pagina, deslocamento);
+   // conteudo = atoi();
 }
